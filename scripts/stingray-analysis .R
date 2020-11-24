cb921ffc3c4f598b51876b8ee600a8caffed165a
hhochel <hhochel@uw.edu>
2020-11-23 19:34:09 -0800

    ran ggplot

--- scripts/stingray-analysis .R
+++ scripts/stingray-analysis .R
@@ -10,3 +10,6 @@ tibble(stingray)
 
 # looking at sex and swimming behavior
 swimming_behavior <- select(stingray, Sex, Swimming.50.Hz, Swimming.90.Hz, Swimming.200.Hz, Swimming.500.Hz, Swimming.1000.Hz, Swimming.10000.Hz)
+
+# run ggplot
+library(ggplot2)
